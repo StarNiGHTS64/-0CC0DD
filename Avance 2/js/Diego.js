44026567f6308168fa9d72b0442c90757c79f9b8
DiegoRz <diego_rz2@hotmail.com>
2018-10-21 10:21:29 -0500

    Modificar registro con formato y funcionalidad

--- Avance 2/js/Diego.js
+++ Avance 2/js/Diego.js
@@ -1,1 +1,12 @@
   
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
+ $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
