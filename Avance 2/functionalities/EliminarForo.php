7dafb591cf744c7aa3da44424b505aabda4aa7d7
Carlos Becerra <A01153661@itesm.mx>
2018-10-22 09:05:50 -0500

    Requisitos Foro php

--- Avance 2/functionalities/EliminarForo.php
+++ Avance 2/functionalities/EliminarForo.php
@@ -1,3 +1,9 @@
 <?php
     session_start();
+    $connect = mysqli_connect("localhost", "root", "", "Lab18");
+	$sql = "DELETE FROM tbl_sample WHERE id = '".$_POST["id"]."'";  
+	if(mysqli_query($connect, $sql))  
+	{  
+		echo 'Entrada Eliminada';  
+	}  
 ?>
--- Avance 2/functionalities/RegistrarForo.php
+++ Avance 2/functionalities/RegistrarForo.php
@@ -1,3 +1,9 @@
 <?php
     session_start();
+    $connect = mysqli_connect("localhost", "root", "", "Lab18");
+    $sql = "INSERT INTO tbl_sample(first_name, last_name) VALUES('".$_POST["first_name"]."', '".$_POST["last_name"]."')";  
+    if(mysqli_query($connect, $sql))  
+    {  
+        echo 'Entrada Registrada';  
+    }  
 ?>
