def23c75dade87a5307cbac7d4a53a57e6669160
Dannytj94 <42280831+Dannytj94@users.noreply.github.com>
2018-10-23 04:12:31 -0500

    modificar atributos

--- Avance 3/js/DannyJS.js
+++ Avance 3/js/DannyJS.js
@@ -1,5 +1,24 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.btn');
-    var instances = M.Collapsible.init(elems, options);
+
+<script>
+    
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+         $(document).ready(function(){
+    $('.tooltipped').tooltip();
   });
 
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+      $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+</script>
