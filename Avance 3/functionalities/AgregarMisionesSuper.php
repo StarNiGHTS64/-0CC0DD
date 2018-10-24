e850f974c4d35de141e8915f1818614c451aaae8
Prkizir <A01020382@itesm.mx>
2018-10-23 22:20:11 -0500

    gawd whyyyy

--- Avance 3/functionalities/AgregarMisionesSuper.php
+++ Avance 3/functionalities/AgregarMisionesSuper.php
@@ -1,3 +1,6 @@
 <?php
-    function insertMission($idMision,$nombre,$descripcion)
+    session_start();
+    include("../header-footer/_header.html");
+    include("_agregaMisionesSuper.html");
+    include("../header-footer/_footer.html")
 ?>
