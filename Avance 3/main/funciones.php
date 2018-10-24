c865541f758e7dfda2418f28d659f99aa8a0169c
DiegoRz <diego_rz2@hotmail.com>
2018-10-23 22:53:27 -0500

    validar usuarios e iniciar requisitos Diego

---
+++ Avance 3/main/funciones.php
@@ -0,0 +1,76 @@
+<?php
+    
+    function connect() {
+                                //servidor_bd, usuario, passwd, nombre_bd
+        $conexion = mysqli_connect("localhost","root","","mundoyoto");
+        
+        if($conexion == NULL) {
+            die("Error al conectarse con la base de datos". mysqli_connect_error());
+        }
+        
+        $conexion->set_charset("utf8");
+        return $conexion;
+    }
+    
+    function disconnect($conexion) {
+        mysqli_close($conexion);
+    }
+    
+    function login($usuario, $contrasena) {
+        $conexion = connect();
+        
+        $usuario = $conexion->real_escape_string($usuario);
+        $contrasena = $conexion->real_escape_string($contrasena);
+        
+         //Specification of the SQL query
+        $query='SELECT usuario FROM `usuario` WHERE usuario = "'.$usuario.
+                '" AND contrasena = "'.$contrasena.'"';
+         // Query execution; returns identifier of the result group
+         
+        $results = $conexion->query($query);
+         // cycle to explode every line of the results
+        while ($row = mysqli_fetch_array($results, MYSQLI_BOTH)) {
+                                        // Options: MYSQLI_NUM to use only numeric indexes
+                                        //          MYSQLI_ASSOC to use only name (string) indexes
+                                        //          MYSQLI_BOTH, to use both
+
+            mysqli_free_result($results);
+            disconnect($conexion);
+            return $row["usuario"];
+        }
+        // it releases the associated results
+        mysqli_free_result($results);
+        disconnect($conexion);
+        return false;
+    }
+    
+    function crear_cuenta($usuario, $contrasena) {
+        $conexion = connect();
+        
+        // insert command specification 
+        $query='INSERT INTO usuario (nombre,contrasena) VALUES (?,?) ';
+        // Preparing the statement 
+        if (!($statement = $conexion->prepare($query))) {
+            disconnect($conexion); 
+            die("Preparation failed: (" . $mysql->errno . ") " . $mysql->error);
+        }
+        // Binding statement params 
+        $usuario = $conexion->real_escape_string($usuario);
+        $contrasena = $conexion->real_escape_string($contrasena);
+            
+        if (!$statement->bind_param("ss", $usuario, $contrasena)) {
+            disconnect($conexion); 
+            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error); 
+        }
+         // Executing the statement
+         if (!$statement->execute()) {
+            disconnect($conexion); 
+            return false;
+          } 
+        
+        disconnect($conexion);
+        return true;
+    }
+
+    
+?>
--- Avance 3/main/index.php
+++ Avance 3/main/index.php
@@ -1,5 +1,52 @@
+
 <?php
-    include("../header-footer/_header.html");
-    include("Yoto-Main.html");
+    session_start();
+
+    require("funciones.php");
+    require_once("util.php");
+
+    //Limpiar datos
+    if (isset($_POST["usuario"])) {
+        $_POST["usuario"] = limpia_entrada($_POST["usuario"]);
+    }
+    
+    if (isset($_POST["contrasena"])) {
+        $_POST["contrasena"] = limpia_entrada($_POST["contrasena"]);
+    }
+    
+
+
+    if (isset($_SESSION["usuario"])) {
+        $_SESSION["mensaje"]='<h3> BIENVENDO'.$_SESSION["usuario"].'</h3>';
+        include("../header-footer/_header.html");
+        include("Yoto-Main.html");
+        
+    } else if (isset($_POST["usuario"]) && login($_POST["usuario"], $_POST["contrasena"]) == true) {
+        echo "Hola";
+        $_SESSION["usuario"] = $_POST["usuario"];
+            
+        include("../header-footer/_header.html");
+        include("Yoto-Main.html");
+        
+    } else if (isset($_POST["usuario"]) && $_POST["usuario"] == "" && $_POST["contrasena"] == "" 
+                && isset($_POST["usuario"])  && isset($_POST["contrasena"]) ) {
+        
+        $error = "Ingresa tu usuario y contraseña";
+            include("../header-footer/_header.html");
+            include("Yoto-Main.html");
+        
+    } else if(isset($_POST["usuario"]) || isset($_POST["contrasena"]) ) {
+        
+        sleep(3);
+        $error = "Usuario y/o contraseña incorrectos";
+        include("../header-footer/_header.html");
+        include("Yoto-Main.html");
+        
+    } else {
+        include("../header-footer/_header.html");
+        include("Yoto-Main.html");
+    }
+    
+ 
     include("../header-footer/_footer.html");
 ?>
