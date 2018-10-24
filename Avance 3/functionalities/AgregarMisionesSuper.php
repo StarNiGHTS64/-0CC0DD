84daf28c857d76fd543cfde7ccd17358acc906c7
Prkizir <A01020382@itesm.mx>
2018-10-23 23:02:50 -0500

    small change

--- Avance 3/functionalities/AgregarMisionesSuper.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    include("../header-footer/_header.html");
-    include("_agregaMisionesSuper.html");
-    include("../header-footer/_footer.html")
-?>
---
+++ Avance 3/functionalities/SuperuserMain.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+    include("../header-footer/_header.html");
+    include("_superuserMain.htm");
+    include("../header-footer/_footer.html");
+?>
