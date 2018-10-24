485882e0e28003cd18c9a7cac1d19b5d046afffe
Prkizir <A01020382@itesm.mx>
2018-10-24 02:34:05 -0500

    Requisite: add mission. check. missing proper style***

--- Avance 3/functionalities/SuperuserMain.php
+++ Avance 3/functionalities/SuperuserMain.php
@@ -1,6 +1,25 @@
 <?php
     session_start();
+    
+    require_once("../main/funciones.php");
+    require("../main/util.php");
+    
     include("../header-footer/_header.html");
-    include("_superuserMain.htm");
+    include("_superuserMain.html");
     include("../header-footer/_footer.html");
+
+    insert_mission();
+
+
+    function insert_mission(){
+        $idMision = $_POST['idMision'];
+        $nombre = $_POST['nombre'];
+        $descripcion = $_POST['descripcion'];
+        
+        echo $idMision;
+        echo $nombre;
+        echo $descripcion;
+        
+        add_mision($idMision,$nombre,$descripcion);
+    }
 ?>
