9632723d4caea1cef7f53710a61a1ad6a150ec18
Dannytj94 <42280831+Dannytj94@users.noreply.github.com>
2018-11-06 13:57:44 -0600

    atributos

--- Avance 3/js/dannyjs.js
+++ Avance 3/js/dannyjs.js
@@ -1,80 +1,42 @@
- $(document).ready(function DropDownGrupos(){
-        $.get('ModificarCompetencia.php',{
-            'idMaestro':1,
-        }).done(function(datos){
-            var aux = document.getElementById("selectGrupo");
-            var data = JSON.parse(datos);
-            var str="<option value='' disabled selected>Selecciona un grupo</option>";
-            for (var i=0; i<data.length;i++){
-                 str += "<option value='"+data[i].idGrupo+"'>"+data[i].nombre+"</option>";
-            }
-            console.log(aux);
-            aux.innerHTML=str;
-            $('select').formSelect();
-        });
-    });
-        
-  $(document).ready(function(){
+
+<script> 
+    
+$(document).ready(function(){
     $('.sidenav').sidenav();
   });
-
- 
-
- $(document).ready(function DropDownEquipos(){
-        $.get('ModificarCompetencia.php',{
-            'idGrupo':1,
-        }).done(function(data){
-            var aux = document.getElementById("selectEquipo");
-            var data = JSON.parse(data);
-            var str="<option value='' disabled selected>Selecciona Equipo</option>";
-            for (var i=0; i<data.length;i++){
-                 str += "<option value='"+data[i].idEquipo+"'>"+data[i].nombre+"</option>";
-            }
-            console.log(aux);
-            aux.innerHTML=str;
-            $('select').formSelect();
-        });
-    });
         
-  $(document).ready(function DropDownAtributo(){
-    $('.sidenav').sidenav();
-  });  
+         $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
 
- $(document).ready(function DropDownEquipos(){
-        $.get('ModificarCompetencia.php',{
-            'idGrupo':1,
-        }).done(function(data){
-            
-            if (aux=1){
-               var aux = document.getElementById("selectAtributo");
-            var data = JSON.parse(data);
-            var str="<option value='' disabled selected>Selecciona atributo</option>";
-            for (var i=0; i<data.length;i++){
-                 str += "<option value='"+data[i].idEquipo+"'>"+data[i].nombre+"</option>";
-            }
-            console.log(aux);
-            aux.innerHTML=str;
-            $('select').formSelect();  
-                
-                
-            } else {
-                 var aux = document.getElementById("selectAtributo");
-            var data = JSON.parse(data);
-            var str="<option value='' disabled selected>Selecciona atributo</option>";
-            for (var i=0; i<data.length;i++){
-                 str += "<option value='"+data[i].idEquipo+"'>"+data[i].nombre+"</option>";
-            }
-            console.log(aux);
-            aux.innerHTML=str;
-            $('select').formSelect();
-                
-                   
-            }
-
-            
-        });
-    });
-        
   $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });  
+    $('.parallax').parallax();
+  });
+        
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+      $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+
+function getRequestObject() {
+  // Asynchronous objec created, handles browser DOM differences
+
+  if(window.XMLHttpRequest) {
+    // Mozilla, Opera, Safari, Chrome, IE 7+
+    return (new XMLHttpRequest());
+  }
+  else if (window.ActiveXObject) {
+    // IE 6-
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } else {
+    // Non AJAX browsers
+    return(null);
+  }
+}
+
+
+<\script>
