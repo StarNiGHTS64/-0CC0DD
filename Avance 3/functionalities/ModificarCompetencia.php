b59100ce0d154b692b6d3441ba7e5e7de82e5764
Dannytj94 <42280831+Dannytj94@users.noreply.github.com>
2018-11-10 14:00:56 -0600

    Prueba

--- Avance 3/functionalities/ModificarCompetencia.php
+++ Avance 3/functionalities/ModificarCompetencia.php
@@ -3,13 +3,8 @@ session_start();
 require_once ("util_danny.php");
 
    
-    $datos = getGrupodeMaestro($_GET['idMaestro']);
-    $data = getEquipodeGrupo($_GET['idEquipo']);
-    echo $datos, $data; 
-    
+  
 
-/*    $data = getEquipodeGrupo($_GET['idEquipo']);
-    echo $data; */
 
 
     $request = $_GET["drop"];
