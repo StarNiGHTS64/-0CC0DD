9962ad9b174573a59614333eb03cfdf1549ec928
DiegoRz <diego_rz2@hotmail.com>
2018-11-12 18:19:43 -0600

    Realizar cambios en vista de historias

--- Avance 3/lands/Hpcuatetex.php
+++ Avance 3/lands/Hpcuatetex.php
@@ -3,4 +3,16 @@
 	include("../header-footer/_header.html");
 	include("_hpcuatetex.html");
     include("../header-footer/_footer.html");
+
+    if (isset($_POST['key'])) {
+
+		$conn = new mysqli('localhost', 'root', '', 'mundoyoto';
+    
+        
+    
+    
+    }
+    
+
+
 ?>
