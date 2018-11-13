96017b51a505de73cfcdc78d9b8c628b1d39d709
DiegoRz <diego_rz2@hotmail.com>
2018-11-13 02:09:56 -0600

    Modificar html tierras para desplegar consultas. incompleto

--- Avance 3/lands/Hpcuatetex.php
+++ Avance 3/lands/Hpcuatetex.php
@@ -4,15 +4,65 @@
 	include("_hpcuatetex.html");
     include("../header-footer/_footer.html");
 
-    if (isset($_POST['key'])) {
+	if (isset($_POST['key'])) {
 
-		$conn = new mysqli('localhost', 'root', '', 'mundoyoto';
-    
+		$conn = new mysqli('localhost', 'root', '', 'mundoyoto');
         
-    
-    
-    }
-    
+        if ($_POST['key'] == 'getRowData') {
+            
+			$rowID = $conn->real_escape_string($_POST['rowID']);
+			$sql = $conn->query("SELECT  tierra, titulo, autor, descripcion FROM historia WHERE idHistoria ='$rowID'");
+			$data = $sql->fetch_array();
+			$jsonArray = array(
+                'tierra' => $data['tierra'],
+				'titulo' => $data['titulo'],
+				'autor' => $data['autor'],
+				'descripcion' => $data['descripcion'],
+			);
 
+			exit(json_encode($jsonArray));
+ 		}
 
+		if ($_POST['key'] == 'getExistingData') {
+			$start = $conn->real_escape_string($_POST['start']);
+			$limit = $conn->real_escape_string($_POST['limit']);
+
+			$sql = $conn->query("SELECT idHistoria, tierra, titulo FROM historia LIMIT $start, $limit");
+			if ($sql->num_rows > 0) {
+				$response = "";
+				while($data = $sql->fetch_array()) {
+					$response .= '
+						<tr>
+							<td>'.$data["idHistoria"].'</td>
+                            
+                            <td id="tierra_'.$data["idHistoria"].'">'.$data["tierra"].'</td>
+                            
+							<td id="titulo_'.$data["idHistoria"].'">'.$data["titulo"].'</td>
+                            
+							<td>
+								<input type="button" onclick="viewORedit('.$data["idHistoria"].', \'edit\')" value="Editar" class="btn btn-primary">
+								<input type="button" onclick="viewORedit('.$data["idHistoria"].', \'view\')" value="Ver" class="btn">
+								<input type="button" onclick="deleteRow('.$data["idHistoria"].')" value="Eliminar" class="btn btn-danger">
+							</td>
+						</tr>
+					';
+				}
+				exit($response);
+			} else
+				exit('reachedMax');
+		}
+
+		$rowID = $conn->real_escape_string($_POST['rowID']);
+
+		if ($_POST['key'] == 'deleteRow') {
+			$conn->query("DELETE FROM historia WHERE idHistoria='$rowID'");
+			exit('¡La fila fue eliminada con éxito!');
+		}
+
+		$titulo = $conn->real_escape_string($_POST['titulo']);
+        $tierra = $conn->real_escape_string($_POST['tierra']);
+		$autor = $conn->real_escape_string($_POST['autor']);
+		$descripcion = $conn->real_escape_string($_POST['descripcion']);
+
+	}
 ?>
