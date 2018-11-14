64b244bf07e3b097c9bcd249f526c9dd18c01bed
Prkizir <A01020382@itesm.mx>
2018-11-13 23:50:19 -0600

    chepo.js modify

--- Avance 3/js/chepo.js
+++ Avance 3/js/chepo.js
@@ -1,36 +1,53 @@
 $(document).ready(function(event){
     
-    //Initialize Form
-    $('.materialSelect').formSelect();
+    writbl();
+    upl_tarea();
     
-    //Add event listener for select
+    $('#dispTareas').on('update',function(){});
+
+    $('.materialSelect').formSelect();
+
     $('.materialSelect').on('contentChanged',function(){
         $(this).formSelect();
     });
-            
-    //Update Select Option Event
-    $.post("Queries/Q_Competencias.php",{"Response":"tarea-select-call"},function(data){
-        //alert(data); //Debug Alert
-
-        $('#select-competencia').append(data); //Append returned values from php query
-
-        $('#select-competencia').trigger('contentChanged'); //Call on event trigger
-    });
-    
-    $('#dispTareas').on('update',function(){
-        //Empty
+        
+    $(window).keydown(function(event){
+        if(event.keyCode == 13){
+            event.preventDefault();
+            return false;
+        } 
     });
     
     function writbl(){
-        //Write table into div
-        $.post("Queries/Q_Tareas.php",{"Response":"lol"},function(data){
-            //alert(data);
+            $.post("Queries/Q_Tareas.php",{"Response":"lol"},function(data){
             $("#dispTareas").html(data);
             $('#dispTareas').trigger('update');
         });
     }
+      
+    //Upload Tarea
+    function upl_tarea(){
+        $("#upl-tarea").submit(function(){
+            var $form = $(this),
+                nameTar = $form.find("input[name='nombreTarea']").val(),
+                descTar = $form.find("textarea[name='descripcionTarea']").val(),
+                idDesc = $form.find("option:selected").val();
+            $.post( "Queries/Q_Tareas.php", {"Response": [nameTar,descTar,idDesc,"uploadTar"]}).done(function( data ) {    
+            });
+        });
+    }
     
-    writbl();
+    //Submit Competencia
+    $("#upl-competencia").submit(function(){
+        
+        var $form = $(this),
+            nameComp = $form.find("input[name='nombreCompetencia']").val(),
+            descComp = $form.find("textarea[name='descripcionCompetencia']").val();
+        $.post( "Queries/Q_Competencias.php", {"Response": [nameComp,descComp,"uploadComp"]}).done(function( data ) {
+                //alert( "Data Loaded: " + data );
+        });
+    });
+    
     
     //CRUD button: Action for delete
     $('body').on("click",".something2",function(){
@@ -47,49 +64,19 @@ $(document).ready(function(event){
     $('body').on("click",".something1",function(){
         var $buttonVal = $(this),
             idTar = $(this).attr("name");
-        $.post("Queries/Q_Tarea.php",{"Response":[idTar,"editTar"]}).done(function(data){
+            $.post("Queries/Q_Tarea.php",{"Response":[idTar,"editTar"]}).done(function(data){
             //alert(data);
             $("#editTareaModal").html(data);
             //writbl();
         });
         //upl_tarea();
     });
-        
     
-    //Prevent normal form upload
-    $(window).keydown(function(event){
-        if(event.keyCode == 13){
-            event.preventDefault();
-            return false;
-        } 
+    
+    //Update Select Option Event
+    $.post("Queries/Q_Competencias.php",{"Response":"tarea-select-call"},function(data){
+        $('#select-competencia').append(data);
+        $('#select-competencia').trigger('contentChanged'); 
     });
-      
-    upl_tarea();
     
-    function upl_tarea(){
-    //Submit Tarea
-        $("#upl-tarea").submit(function(){
-
-            var $form = $(this),
-                nameTar = $form.find("input[name='nombreTarea']").val(),
-                descTar = $form.find("textarea[name='descripcionTarea']").val(),
-                idDesc = $form.find("option:selected").val();
-                //alert(idDesc);
-            $.post( "Queries/Q_Tareas.php", {"Response": [nameTar,descTar,idDesc,"uploadTar"]}).done(function( data ) {
-                //alert( "Data Loaded: " + data );
-                //Empty
-            });
-        });
-    }
-    
-    //Submit Competencia
-    $("#upl-competencia").submit(function(){
-        
-        var $form = $(this),
-            nameComp = $form.find("input[name='nombreCompetencia']").val(),
-            descComp = $form.find("textarea[name='descripcionCompetencia']").val();
-        $.post( "Queries/Q_Competencias.php", {"Response": [nameComp,descComp,"uploadComp"]}).done(function( data ) {
-                //alert( "Data Loaded: " + data );
-        });
-    });
 });
