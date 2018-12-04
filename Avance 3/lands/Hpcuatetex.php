d4f03cd3734bdc34bf071481966db5932d102cd0
Dannytj94 <A01172848@itesm.mx>
2018-12-04 00:51:37 -0600

    Corrigiendo errores

--- Avance 3/lands/Hpcuatetex.php
+++ Avance 3/lands/Hpcuatetex.php
@@ -4,15 +4,7 @@
 	include("_hpcuatetex.html");
     include("../header-footer/_footer.html");
 
-    if (isset($_POST['key'])) {
-
-		$conn = new mysqli('localhost', 'root', '', 'mundoyoto';
-    
-        
-    
-    
-    }
-    
+  
 
 
 ?>
